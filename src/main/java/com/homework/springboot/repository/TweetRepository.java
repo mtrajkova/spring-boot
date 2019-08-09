d9a40e04eea2c65cd993406498e45d41eff25a0d
Marija Trajkova <tmarija2008@yahoo.com>
2019-08-09 12:20:08 +0200

    Add repository layer for User and Tweet

---
+++ src/main/java/com/homework/springboot/repository/TweetRepository.java
@@ -0,0 +1,10 @@
+package com.homework.springboot.repository;
+
+import com.homework.springboot.model.Tweet;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TweetRepository extends JpaRepository<Tweet, Long> {
+
+}
